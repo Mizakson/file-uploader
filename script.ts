1f3a202492aaedf91e581162fbf609f40256f618
Mizakson <mizakson.dev@gmail.com>
2025-03-05 12:09:35 -0500

    add main function to script.ts

--- script.ts
+++ script.ts
@@ -2,3 +2,15 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 // use `prisma` in your application to read and write data in your DB
+
+async function main() {
+    // queries go here
+}
+
+main()
+    .catch(e => {
+        console.error(e.message)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
