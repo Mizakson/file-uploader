05f0e67954085cf1cee20163a66a467107462fe9
Mizakson <mizakson.dev@gmail.com>
2025-03-12 12:54:41 -0400

    Add passport localStrategy

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express")
 const session = require("express-session")
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store")
 const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+const passport = require("passport")
+const localStrategy = require("passport-local").Strategy
+
 const app = express()
 
 // routers here
@@ -28,8 +32,48 @@ app.use(
         )
     })
 )
+app.use(passport.session())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-// require("./config/passport")(passport)
+passport.use(
+    new localStrategy(async (username, password, done) => {
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    name: username
+                }
+            })
+
+            if (!user) return done(null, false, { message: "Incorrect username..." })
+
+            const match = await bcrypt.compare(password, user.password)
+            if (!match) return done(null, false, { message: "Incorrect password..." })
+        } catch (err) {
+            return done(err)
+        }
+    })
+)
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: id
+            }
+        })
+
+        console.log(user)
+        done(null, user)
+    } catch (err) {
+        done(err)
+    }
+})
+
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user
