213953a7dca08dcb42eb3e79323fc8a23c096dc2
Mizakson <mizakson.dev@gmail.com>
2025-03-13 15:11:59 -0400

    Add createNewUser method to post request (line 6)

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,6 @@ const { Router } = require("express")
 const userController = require("../controllers/userController")
 const userRouter = Router()
 
-userRouter.post("sign-up", userController.validateUser)
+userRouter.post("sign-up", userController.validateUser, userController.createNewUser)
 
 module.exports = userRouter
