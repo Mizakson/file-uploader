ba7c7822b5bb205f0de49b27ccc315b544ec1c48
Mizakson <mizakson.dev@gmail.com>
2025-03-15 14:33:59 -0400

    Add import statements

--- routes/contentRouter.js
+++ routes/contentRouter.js
@@ -3,4 +3,8 @@ const { Router } = require("express")
 const contentController = require("../controllers/contentController")
 const contentRouter = Router()
 
-// fileRouter.post("/upload-file", middleware here)
+// contentRouter.post("/upload-file", middleware here)
+
+// contentRouter.post("add-folder", middleware here)
+
+module.exports = contentRouter
