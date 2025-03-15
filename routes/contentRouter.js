fb36a99d82908bf184d07fd6a8e11c6033609f79
Mizakson <mizakson.dev@gmail.com>
2025-03-15 14:32:44 -0400

    Refactor to contentRouter, delete folderRouter

---
+++ routes/contentRouter.js
@@ -0,0 +1,6 @@
+const { Router } = require("express")
+
+const contentController = require("../controllers/contentController")
+const contentRouter = Router()
+
+// fileRouter.post("/upload-file", middleware here)
