2cd057f5838fed70f12f8501b96a6c4c08e0776e
Mizakson <mizakson.dev@gmail.com>
2025-07-31 15:23:32 -0400

    Edit formatting

--- routes/contentRouter.js
+++ routes/contentRouter.js
@@ -1,10 +1,8 @@
 const { Router } = require("express")
 const contentRouter = Router()
 
-// Import the new controller
 const contentController = require('../controllers/contentController')
 
-// Assign controller functions to each route
 contentRouter.post("/folder/:folderId/upload-file", contentController.uploadMiddleware, contentController.uploadFile)
 contentRouter.post("/add-folder", contentController.addFolder)
 contentRouter.get("/:folderId/edit-folder", contentController.getEditFolder)
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,8 @@
 const { Router } = require("express")
 const router = Router()
 
-// Import the new controller
 const indexController = require("../controllers/indexController")
 
-// Assign controller functions to each route
 router.get("/", indexController.getIndex)
 router.get("/sign-up", indexController.getSignUp)
 router.get("/login", indexController.getLogin)
